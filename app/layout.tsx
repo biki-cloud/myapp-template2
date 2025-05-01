30c01000c2fff1721b0692f8060754e679ef6253
hibiki <hibiki123123@gmail.com>
2025-05-02 07:17:41 +0900

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "My App",
+  title: "My App ",
   description: "My App Description",
 };
 
